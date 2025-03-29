b3fb41262321162f7a234e57fe132c94ec109fb0
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2025-03-29 06:03:00 -0700

    html_factory

---
+++ src/html.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "oxygen.hpp"
+
+namespace oxygen
+{
+	
+ElementFactories html_factory()
+{
+	ElementFactories result;
+
+    auto button_factory = TextFactory( R"(
+        <button type="{{type}}">{{content}}</button>
+    )")
+    .set_var("type", "submit");
+
+    result.insert("button", button_factory);
+
+    auto radio_button_factory = TextFactory(R"(
+        <div>
+            <input type="{{type}}" id="{{id}}" name="{{name}}" value="{{value}}" {{checked}}/>
+            <label for="{{id}}">{{label}}</label>
+        </div>
+    )")
+    .set_vars({
+        { "checked", "" },
+        { "type", "radio" },
+    });
+    result.insert("radio_button", radio_button_factory);
+
+    auto form_factory = TextFactory(R"(
+        <form action="{{action}}">
+        {{content}}
+        {{submit}}
+        </form>
+    )")
+    .set_var("submit", "");
+    result.insert("form", form_factory);
+
+    return result;
+}
+
+}
