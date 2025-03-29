7decc23dbf32360d8200724e543617dbb6cce957
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2025-03-29 09:32:38 -0700

    update text_utils

--- test/xc-build/build/example.build/Debug/Objects-normal/arm64/example.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/mucbuc/work/wgui/test/lib/oxygen/test/src/example.cpp
--- test/xc-build/build/oxygen.build/Debug/Objects-normal/arm64/oxygen.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/mucbuc/work/wgui/test/lib/oxygen/test/lib/oxygen/src/oxygen.cpp \
-  /Users/mucbuc/work/wgui/test/lib/oxygen/test/lib/oxygen/src/oxygen.hpp
